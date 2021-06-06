25720f93c2ca425ebf5a4e485405d790dd153ab1
Timo Reitz <godsboss@gmx.de>
2021-06-06 21:09:32 +0200

    Formatting...

--- pkg/domain/domain.go
+++ pkg/domain/domain.go
@@ -1,11 +1,11 @@
 package domain
 
 type Calculation interface {
-  Calculate() (int64, error)
+	Calculate() (int64, error)
 }
 
 type Const int64
 
 func (cnst Const) Calculate() (int64, error) {
-  return int64(cnst), nil
+	return int64(cnst), nil
 }
--- pkg/domain/domain_test.go
+++ pkg/domain/domain_test.go
@@ -1,21 +1,21 @@
 package domain_test
 
 import (
-  "github.com/GodsBoss/gh-actions-experiment/pkg/domain"
+	"github.com/GodsBoss/gh-actions-experiment/pkg/domain"
 
-  "testing"
+	"testing"
 )
 
 func TestConst(t *testing.T) {
-  cnst := domain.Const(100)
+	cnst := domain.Const(100)
 
-  result, err := cnst.Calculate()
+	result, err := cnst.Calculate()
 
-  if err != nil {
-    t.Errorf("expected no error, got %+v", err)
-  }
+	if err != nil {
+		t.Errorf("expected no error, got %+v", err)
+	}
 
-  if result != 100 {
-    t.Errorf("expected result to be 100, got %d", result)
-  }
+	if result != 100 {
+		t.Errorf("expected result to be 100, got %d", result)
+	}
 }
